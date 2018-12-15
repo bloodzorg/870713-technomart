f5f43a2983c621a005a04f0624fa1b16d49509c8
bloodzorg <44965782+bloodzorg@users.noreply.github.com>
2018-12-15 14:22:13 +1000

    Сделал карту

--- js/script.js
+++ js/script.js
@@ -14,9 +14,10 @@ mapClose.addEventListener("click", function (evt) {
 
 
 window.addEventListener("keydown", function (evt) {
+  evt.preventDefault();
   if (evt.keyCode === 27) {
-    evt.preventDefault();
     if (mapPopup.classList.contains("modal-show")) {
       mapPopup.classList.remove("modal-show");
+    }
   }
-}
+});
