866c7769174d2202210ffa80645e79b1b43f6077
bloodzorg <44965782+bloodzorg@users.noreply.github.com>
2018-12-16 12:41:57 +1000

    Выполнил задание

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var mapLink = document.querySelector(".contacts-img-map");
 var mapPopup = document.querySelector(".modal-map");
 var mapClose = mapPopup.querySelector(".modal-map-close");
+var Link = document.querySelector(".about-us-btn");
+var Popup = document.querySelector(".modal-write-us");
+var Close = Popup.querySelector(".modal-write-us-close");
 
 mapLink.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -18,6 +21,49 @@ window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (mapPopup.classList.contains("modal-show")) {
       mapPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+Link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  Popup.classList.add("modal-show");
+});
+
+Close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  Popup.classList.remove("modal-show");
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    if (Popup.classList.contains("modal-show")) {
+      Popup.classList.remove("modal-show");
+    }
+  }
+});
+
+var cartLink = document.querySelector(".buy-button");
+var cartPopup = document.querySelector(".modal-in-cart");
+var cartClose = cartPopup.querySelector(".modal-in-cart-close");
+
+cartLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.add("modal-show");
+});
+
+cartClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  cartPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  evt.preventDefault();
+  if (evt.keyCode === 27) {
+    if (cartPopup.classList.contains("modal-show")) {
+      cartPopup.classList.remove("modal-show");
     }
   }
 });
